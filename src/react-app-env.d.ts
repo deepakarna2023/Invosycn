0215dce7c0921a06bfc094979c5529f81e6f2a87
deepakarna2023 <deepak.sirsath@arnasoftech.com>
2025-01-10 13:00:06 +0530

    changes into the json server config

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react-scripts" />
+
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+      REACT_APP_SERVER_PORT: string;
+    }
+  }
--- src/services/axiosConfiguration.ts
+++ src/services/axiosConfiguration.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+const port = process.env.REACT_APP_SERVER_PORT;
+console.log(port);
 const axiosInstance = axios.create({
-  baseURL: "/api/",
+  baseURL: `${port}/`,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
